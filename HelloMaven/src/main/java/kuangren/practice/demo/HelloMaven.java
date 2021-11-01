a48fe8e5b579decc9e678cfa0b23b546e0121173
yemanqitipo <yemanqitipo@163.com>
2021-11-02 01:39:56 +0800

    remove name in pom.xml

--- HelloMaven/src/main/java/kuangren/practice/demo/HelloMaven.java
+++ HelloMaven/src/main/java/kuangren/practice/demo/HelloMaven.java
@@ -6,7 +6,7 @@ public class HelloMaven
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello, maven" );
+        System.out.println( "Hello, Maven" );
         
         Foo f = new Foo();
         f.foo();
