4908da39dcda522257aad4c82a4ebd30ae3a4eb9
yemanqitipo <yemanqitipo@163.com>
2021-11-02 01:29:02 +0800

    multi maven module

---
+++ AnnotationDemo/src/main/java/kuangren/practice/demo/Foo.java
@@ -0,0 +1,12 @@
+package kuangren.practice.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class Foo 
+{
+   public void foo() {
+	   System.out.println("this is Foo.foo");
+   }
+}
---
+++ HelloMaven/src/main/java/kuangren/practice/demo/HelloMaven.java
@@ -0,0 +1,14 @@
+package kuangren.practice.demo;
+
+import kuangren.practice.demo.Foo;
+
+public class HelloMaven
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello, maven" );
+        
+        Foo f = new Foo();
+        f.foo();
+    }
+}
